41b71e52f350f0f9c5fc273aad338938ee27c902
yzw <1537770662@qq.com>
2022-07-13 08:54:29 +0800

    ReadMe

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>test02</h1>
+  <img alt="Vue logo" src="./assets/logo_2.0.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+  img{
+    width: 10%;
+    height: 10%;
+  }
+</style>
